6300e16cd50f90b5a64ebdf4ab0a73a46d3fc8d8
javipiazza94 <97734456+javipiazza94@users.noreply.github.com>
2022-02-09 20:49:27 +0100

    Add files via upload

---
+++ EJERCICIOS_PILDORAS_INFORMATICA/ARRAYS_pildoras_info_I.java
@@ -0,0 +1,21 @@
+package EJERCICIOS_PILDORAS_INFORMATICA;
+import java.util.*;
+
+public class ARRAYS_pildoras_info_I {
+
+	public static void main(String[] args) {
+		// Usar arrays
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int [] array = new int [100];
+		
+		for (int i = 0; i<array.length; i++) {
+			array[i] = (int) Math.round(Math.random()*100);
+	}
+		for (int numeros:array) {
+		System.out.println(numeros + " ");
+		}
+		sc.close();
+
+}}
---
+++ EJERCICIOS_PILDORAS_INFORMATICA/clase_II.java
@@ -0,0 +1,5 @@
+package EJERCICIOS_PILDORAS_INFORMATICA;
+
+public class clase_II {
+
+}
