46211b421c970fd477edd0be11a0c7f87ef579fe
Javi Piazza <97734456+javipiazza94@users.noreply.github.com>
2022-12-15 13:23:15 +0100

    Add files via upload

---
+++ PRIMERO_DAM/JAVA-main/AJEDREZ/CLASES_PROPIAS/TIRAR_DADO.java
@@ -0,0 +1,13 @@
+package CLASES_PROPIAS;
+
+public class TIRAR_DADO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dado tirar_dado = new Dado (20);
+		System.out.println(Dado.getncaras());
+		tirar_dado.tirar();
+		System.out.println(Dado.getvalor());
+	}
+
+}
